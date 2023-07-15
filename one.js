4b4cade41c55e30476de164eb8cf659cc016f2ba
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-15 17:42:06 +0530

    Js

---
+++ one.js
@@ -0,0 +1,9 @@
+var x=7
+function getname(){
+
+    console.log("Hii,Good Evening")
+}
+getname()
+console.log(x)
+console.log(getname)
+
---
+++ two.js
@@ -0,0 +1,12 @@
+getname()
+console.log(x)
+console.log(getname)
+
+var x=7
+var getname=()=>{   
+
+    console.log("Hii,Good Evening")
+}
+// getname()
+
+
